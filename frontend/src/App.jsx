7e1dffb1d1279e5305cce03d65a13a4dc3f8d7fb
Pader <paderx26@gmail.com>
2026-03-23 13:24:15 -0500

    added moviecard component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import MovieCard from "./components/MovieCard";
+
+function App() {
+  return;
+  <>
+    <MovieCard movie={{ title: "Film", release_date: "2024" }} />
+  </>;
+}
+
+export default App;

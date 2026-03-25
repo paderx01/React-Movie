a39514c2ee1c28a68d78a9b2026d330e11ef4019
Pader <paderx26@gmail.com>
2026-03-25 13:34:03 -0500

    added page routers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Favorites from "./pages/Favorites";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </main>
   );
 }
 
---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorites() {
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet</h2>
+      <h2>Add movies to your favorites to appear here</h2>
+    </div>
+  );
+}
+
+export default Favorites;

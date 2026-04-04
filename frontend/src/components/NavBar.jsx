9cb4b4ed2f387c7cc9a174f26bc6893457379e46
Pader <paderx26@gmail.com>
2026-04-03 20:01:11 -0500

    debug BrowserRoute

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return;
+  <nav className="navbar">
+    <div classname="navbar-brand"></div>
+    <link to="/">Movie App</link>
+    <div className="navbar-links">
+      <Link to="/" className="nav-link">
+        Home
+      </Link>
+      <Link to="/favorites" className="nav-link">
+        Favorites
+      </Link>
+    </div>
+  </nav>;
+}
+
+export default NavBar;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
